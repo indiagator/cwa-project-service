fe82698b6d57aab48f2f2a9d8c37510e51ed0b1f
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2025-08-22 10:26:07 +0530

    friday commit

--- src/main/java/com/egov/projectservice/MainRestController.java
+++ src/main/java/com/egov/projectservice/MainRestController.java
@@ -1,12 +1,21 @@
 package com.egov.projectservice;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("api/v1")
@@ -19,47 +28,103 @@ public class MainRestController
     TokenService tokenService;
     @Autowired
     private ApplicationContext ctx;
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
     @PostMapping("float/project")
     public ResponseEntity<?> floatProject(@RequestBody Project project,
-                                          @RequestHeader("Authorization") String token)
-    { // AOP - Aspected Oriented Programming - Micrometer does its job here [ trace + span generation]
-        logger.info("Request received to float project: " + project);
-        Principal principal =  tokenService.validateToken(token);
-        if(principal.getState().equals("VALID"))
+                                          @RequestHeader("Authorization") String token,
+                                          HttpServletResponse httpServletResponse,
+                                          HttpServletRequest httpServletRequest)
+    {
+        Cookie[] cookies =  httpServletRequest.getCookies();
+        List<Cookie> cookieList = new ArrayList<>();
+        if(cookies != null && cookies.length > 0)
         {
-            logger.info("Token validated successfully");
-            // Token is valid, proceed with the update
-            if(project.getOwnerPhone().equals(principal.getUsername())) // AUTHORIZATION OF REQUEST HAPPENS HERE
+            cookieList = Arrays.asList(cookies);
+        }
+
+
+       if(cookieList.stream().noneMatch(cookie -> cookie.getName().equals("float_project_stage_1")))
+        {
+            // NO RELEVANT COOKIE FOUND - SO PROCEEDING WITH FRESH REQUEST LOGIC
+
+            // AOP - Aspected Oriented Programming - Micrometer does its job here [ trace + span generation]
+            logger.info("Request received to float project: " + project);
+            Principal principal =  tokenService.validateToken(token);
+            if(principal.getState().equals("VALID"))
             {
-                logger.info("Phone number matches with the token");
-                project.setStatus("FLOATED");
-                projectRepository.save(project);
-                logger.info("Project floated successfully in the database: " + project);
-                // Forward an ASYNC Request to the Comm-Service to send messages to the Contractors
-                WebClient commServiceWebClient = ctx.getBean("commServiceWebClient", WebClient.class);
+                logger.info("Token validated successfully");
+                // Token is valid, proceed with the update
+                if(project.getOwnerPhone().equals(principal.getUsername())) // AUTHORIZATION OF REQUEST HAPPENS HERE
+                {
+                    logger.info("Phone number matches with the token");
+                    project.setStatus("FLOATED");
+                    projectRepository.save(project);
+                    logger.info("Project floated successfully in the database: " + project);
+                    // Forward an ASYNC Request to the Comm-Service to send messages to the Contractors
 
-                commServiceWebClient.post()
-                        .header("Authorization", token)
-                        .bodyValue(project)
-                        .retrieve()
-                        .bodyToMono(String.class)
-                        .subscribe(response -> logger.info("Notification sent to contractors: " + response),
-                                error -> logger.error("Error sending notification to contractors: " + error.getMessage()));
+                    String stage1Key = String.valueOf(new Random().nextInt(100000));
+                   redisTemplate.opsForValue().set(stage1Key, "STAGE 1 COMPLETE | STAGE 2 IN PROGRESS", 30); // Store in Redis for 30 seconds
 
-                return  ResponseEntity.ok("Project Floated Successfully");
+                    Cookie cookie = new Cookie("float_project_stage_1", stage1Key);
+
+
+                    WebClient commServiceWebClient = ctx.getBean("commServiceWebClient", WebClient.class);
+
+                    commServiceWebClient.post()
+                            .header("Authorization", token)
+                            .bodyValue(project)
+                            .retrieve()
+                            .bodyToMono(String.class)
+                            .subscribe(
+
+                                    response ->
+                                    {
+                                        logger.info("Notification sent to contractors: " + response);
+                                        // Add more logic here if needed
+                                        redisTemplate.opsForValue().set(stage1Key, "STAGE 1 COMPLETE | STAGE 2 COMPLETE | TRANSACTION COMPLETE", 30);
+                                    },
+
+
+
+                                    error -> logger.error("Error sending notification to contractors: " + error.getMessage())
+
+
+                                      );
+
+
+
+                    httpServletResponse.addCookie(cookie);
+                    return  ResponseEntity.ok("/float/project | STAGE 1 COMPLETE | STAGE 2 IN PROGRESS");
+                }
+                else
+                {
+                    logger.info("Phone number does not match with the token");
+                    return ResponseEntity.status(401).body("Unauthorized: Phone number does not match with the token");
+                }
             }
             else
             {
-                logger.info("Phone number does not match with the token");
-                return ResponseEntity.status(401).body("Unauthorized: Phone number does not match with the token");
+                logger.info("Token not valid");
+                return ResponseEntity.status(401).body("Unauthorized: Invalid Token");
             }
         }
-        else
-        {
-            logger.info("Token not valid");
-            return ResponseEntity.status(401).body("Unauthorized: Invalid Token");
-        }
+       else
+       {
+           // RELEVANT COOKIE FOUND - SO PROCEEDING WITH FOLLOW UP LOGIC
+
+           String cacheValue = (String) redisTemplate.opsForValue().get(cookieList.stream()
+                   .filter(cookie -> cookie.getName().equals("float_project_stage_1"))
+                   .findFirst()
+                   .orElseThrow(() -> new RuntimeException("Cookie not found")).getValue());
+
+           Cookie cookie = new Cookie("float_project_stage_2", "the new redis key for stage 2");
+
+           return ResponseEntity.ok(cacheValue);
+       }
+
+
     }
 
 }
